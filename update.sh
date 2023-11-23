fcac9bb753790cee1a5e50581375cfc64d6d4add
Cole Fuerth <cfuerth@satcomdirect.com>
2023-11-22 20:25:03 -0500

    change update script to always check for new symlinks

--- update.sh
+++ update.sh
@@ -4,18 +4,15 @@ BIN="$HOME/.local/bin"
 ALIASES="$HOME/.zsh_aliases"
 
 if git remote update -p > /dev/null && git status -uno | grep -q 'Your branch is behind'; then
-
-    echo "updating dot-files..."
-
+    echo "dot-files has an update!"
     git pull
-
-    mkdir -p "$ALIASES"
-
-    for file in aliases/*; do
-        destination="$ALIASES/$(basename "$file")"
-        if [ ! -e "$destination" ]; then
-            ln -s "$PWD/$file" "$destination"
-        fi
-    done
-
 fi
+
+mkdir -p "$ALIASES"
+
+for file in aliases/*; do
+    destination="$ALIASES/$(basename "$file")"
+    if [ ! -e "$destination" ]; then
+        ln -s "$PWD/$file" "$destination"
+    fi
+done
