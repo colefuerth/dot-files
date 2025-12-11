16b0b2a9963015bdf541f10a2057625bde329466
Cole Fuerth <colefuerth@gmail.com>
2025-12-10 22:44:44 -0800

    push hyprland changes

--- nixos/common/hyprland.nix
+++ nixos/common/hyprland.nix
@@ -24,19 +24,20 @@ in
       trusted-substituters = [ "https://hyprland.cachix.org" ];
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
-    
+
     # Enable X11 and Wayland support
     services.xserver.enable = true;
     services.displayManager.sddm.enable = true;
     services.displayManager.sddm.wayland.enable = true;
-    
+
     # Enable automatic login for the user
     services.displayManager.autoLogin.enable = true;
     services.displayManager.autoLogin.user = "cole";
-    
+
     environment = {
       systemPackages = [
         pkgs.kitty # required for the default Hyprland config
+        pkgs.adwaita-icon-theme # GNOME cursor theme
       ];
       # Optional, hint Electron apps to use Wayland:
       sessionVariables.NIXOS_OZONE_WL = "1";
@@ -65,8 +66,16 @@ in
         withUWSM = true; # recommended for most users
         xwayland.enable = true; # Xwayland can be disabled.
       };
-    
+
     home-manager.users.cole = {
+      home.pointerCursor = {
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+        size = 24;
+        gtk.enable = true;
+        x11.enable = true;
+      };
+
       wayland.windowManager.hyprland = {
         enable = true;
         package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
@@ -85,6 +94,12 @@ in
             "kitty"
           ];
 
+          # Environment variables
+          env = [
+            "XCURSOR_THEME,Adwaita"
+            "XCURSOR_SIZE,24"
+          ];
+
           # Bindings
           bind = [
             "$mod, Q, exec, kitty"
@@ -99,17 +114,51 @@ in
             "$mod, right, movefocus, r"
             "$mod, up, movefocus, u"
             "$mod, down, movefocus, d"
+
+            # Move windows
+            "$mod SHIFT, left, movewindow, l"
+            "$mod SHIFT, right, movewindow, r"
+            "$mod SHIFT, up, movewindow, u"
+            "$mod SHIFT, down, movewindow, d"
+
+            # Resize windows
+            "$mod CTRL, left, resizeactive, -20 0"
+            "$mod CTRL, right, resizeactive, 20 0"
+            "$mod CTRL, up, resizeactive, 0 -20"
+            "$mod CTRL, down, resizeactive, 0 20"
+
+            # Workspace switching
+            "$mod, 1, workspace, 1"
+            "$mod, 2, workspace, 2"
+            "$mod, 3, workspace, 3"
+            "$mod, 4, workspace, 4"
+            "$mod, 5, workspace, 5"
+
+            # Move window to workspace
+            "$mod SHIFT, 1, movetoworkspace, 1"
+            "$mod SHIFT, 2, movetoworkspace, 2"
+            "$mod SHIFT, 3, movetoworkspace, 3"
+            "$mod SHIFT, 4, movetoworkspace, 4"
+            "$mod SHIFT, 5, movetoworkspace, 5"
           ];
 
           # Window rules
           general = {
             gaps_in = 5;
-            gaps_out = 10;
+            gaps_out = 5;
             border_size = 2;
           };
 
           decoration = {
-            rounding = 10;
+            rounding = 5;
+          };
+
+          input = {
+            touchpad = {
+              natural_scroll = true;
+              tap-to-click = true;
+              disable_while_typing = true;
+            };
           };
         };
       };
