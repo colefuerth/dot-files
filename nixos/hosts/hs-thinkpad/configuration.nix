406b5a3724a527e280b3ee110f5b41af888df9e5
Cole Fuerth <colefuerth@gmail.com>
2026-01-16 16:23:00 -0800

    enable tlp for automatic power management, and battery saving

--- nixos/hosts/hs-thinkpad/configuration.nix
+++ nixos/hosts/hs-thinkpad/configuration.nix
@@ -282,6 +282,25 @@ in
     enable = true;
     powertop.enable = true;
   };
+  services.tlp = {
+    enable = true; # Enable TLP (better than gnomes internal power manager)
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 1;
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 1;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 81;
+    };
+  };
+  services.thermald.enable = true; # Enable thermald, the temperature management daemon. (only necessary if on Intel CPUs)
+  services.power-profiles-daemon.enable = false; # Disable GNOMEs power management
 
   # Use simple graphics configuration like working /etc config
   hardware.graphics = {
