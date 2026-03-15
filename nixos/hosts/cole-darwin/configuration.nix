e599e96c6d567b6d1f828188d2f2e936fb21a546
Cole Fuerth <colefuerth@gmail.com>
2026-03-14 20:36:02 -0700

    fix yabai keybinds

--- nixos/hosts/cole-darwin/configuration.nix
+++ nixos/hosts/cole-darwin/configuration.nix
@@ -165,66 +165,66 @@
       text = ''
         #!/usr/bin/env sh
 
-        /opt/homebrew/bin/yabai -m config layout                 bsp
-        /opt/homebrew/bin/yabai -m config window_placement       second_child
-        /opt/homebrew/bin/yabai -m config top_padding            8
-        /opt/homebrew/bin/yabai -m config bottom_padding         8
-        /opt/homebrew/bin/yabai -m config left_padding           8
-        /opt/homebrew/bin/yabai -m config right_padding          8
-        /opt/homebrew/bin/yabai -m config window_gap             8
-        /opt/homebrew/bin/yabai -m config mouse_follows_focus    off
-        /opt/homebrew/bin/yabai -m config focus_follows_mouse    off
-        /opt/homebrew/bin/yabai -m config mouse_modifier         alt
-        /opt/homebrew/bin/yabai -m config mouse_action1          move
-        /opt/homebrew/bin/yabai -m config mouse_action2          resize
-        /opt/homebrew/bin/yabai -m config window_shadow          float
-        /opt/homebrew/bin/yabai -m config split_ratio            0.5
-        /opt/homebrew/bin/yabai -m config auto_balance           off
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config layout                 bsp
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config window_placement       second_child
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config top_padding            8
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config bottom_padding         8
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config left_padding           8
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config right_padding          8
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config window_gap             8
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config mouse_follows_focus    off
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config focus_follows_mouse    off
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config mouse_modifier         alt
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config mouse_action1          move
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config mouse_action2          resize
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config window_shadow          float
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config split_ratio            0.5
+        /opt/homebrew/bin//opt/homebrew/bin/yabai -m config auto_balance           off
       '';
     };
 
     # skhd hotkey daemon config
     home.file.".skhdrc".text = ''
       # focus window
-      alt - h : yabai -m window --focus west
-      alt - j : yabai -m window --focus south
-      alt - k : yabai -m window --focus north
-      alt - l : yabai -m window --focus east
+      alt - h : /opt/homebrew/bin/yabai -m window --focus west
+      alt - j : /opt/homebrew/bin/yabai -m window --focus south
+      alt - k : /opt/homebrew/bin/yabai -m window --focus north
+      alt - l : /opt/homebrew/bin/yabai -m window --focus east
 
       # swap window
-      shift + alt - h : yabai -m window --swap west
-      shift + alt - j : yabai -m window --swap south
-      shift + alt - k : yabai -m window --swap north
-      shift + alt - l : yabai -m window --swap east
+      shift + alt - h : /opt/homebrew/bin/yabai -m window --swap west
+      shift + alt - j : /opt/homebrew/bin/yabai -m window --swap south
+      shift + alt - k : /opt/homebrew/bin/yabai -m window --swap north
+      shift + alt - l : /opt/homebrew/bin/yabai -m window --swap east
 
       # move window
-      ctrl + alt - h : yabai -m window --warp west
-      ctrl + alt - j : yabai -m window --warp south
-      ctrl + alt - k : yabai -m window --warp north
-      ctrl + alt - l : yabai -m window --warp east
+      ctrl + alt - h : /opt/homebrew/bin/yabai -m window --warp west
+      ctrl + alt - j : /opt/homebrew/bin/yabai -m window --warp south
+      ctrl + alt - k : /opt/homebrew/bin/yabai -m window --warp north
+      ctrl + alt - l : /opt/homebrew/bin/yabai -m window --warp east
 
       # toggle float and center
-      alt - t : yabai -m window --toggle float --grid 4:4:1:1:2:2
+      alt - t : /opt/homebrew/bin/yabai -m window --toggle float --grid 4:4:1:1:2:2
 
       # toggle fullscreen
-      alt - f : yabai -m window --toggle zoom-fullscreen
+      alt - f : /opt/homebrew/bin/yabai -m window --toggle zoom-fullscreen
 
       # balance tree
-      shift + alt - 0 : yabai -m space --balance
+      shift + alt - 0 : /opt/homebrew/bin/yabai -m space --balance
 
       # focus spaces
-      alt - 1 : yabai -m space --focus 1
-      alt - 2 : yabai -m space --focus 2
-      alt - 3 : yabai -m space --focus 3
-      alt - 4 : yabai -m space --focus 4
-      alt - 5 : yabai -m space --focus 5
+      alt - 1 : /opt/homebrew/bin/yabai -m space --focus 1
+      alt - 2 : /opt/homebrew/bin/yabai -m space --focus 2
+      alt - 3 : /opt/homebrew/bin/yabai -m space --focus 3
+      alt - 4 : /opt/homebrew/bin/yabai -m space --focus 4
+      alt - 5 : /opt/homebrew/bin/yabai -m space --focus 5
 
       # move window to space
-      shift + alt - 1 : yabai -m window --space 1
-      shift + alt - 2 : yabai -m window --space 2
-      shift + alt - 3 : yabai -m window --space 3
-      shift + alt - 4 : yabai -m window --space 4
-      shift + alt - 5 : yabai -m window --space 5
+      shift + alt - 1 : /opt/homebrew/bin/yabai -m window --space 1
+      shift + alt - 2 : /opt/homebrew/bin/yabai -m window --space 2
+      shift + alt - 3 : /opt/homebrew/bin/yabai -m window --space 3
+      shift + alt - 4 : /opt/homebrew/bin/yabai -m window --space 4
+      shift + alt - 5 : /opt/homebrew/bin/yabai -m window --space 5
     '';
   };
 }
