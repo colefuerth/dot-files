6126392a33d529069a9a57744c3a738e6c93624c
Cole Fuerth <colefuerth@gmail.com>
2026-04-10 09:36:54 -0700

    remove the cross compile overlays that were causing cache hits

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,8 +5,8 @@ in
 (lib.composeManyExtensions [
   (import ./chromium)
   (import ./flameshot inputs)
-  (import ./fresh-editor)
-  (import ./gh)
+  # (import ./fresh-editor)
+  # (import ./gh)
   # disable SSH overlay and use home-manager to fix ~/.ssh/config perms
   # (import ./openssh)
 ])
