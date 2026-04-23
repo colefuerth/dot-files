b6158258f686afba8dcfff367075ecb9a2e5c6ac
Cole Fuerth <colefuerth@gmail.com>
2026-04-22 19:14:08 -0700

    add tailscale to desktop

--- nixos/hosts/cole-desktop/configuration.nix
+++ nixos/hosts/cole-desktop/configuration.nix
@@ -87,7 +87,6 @@ in
         slack
         slurp
         spotify
-        tailscale-systray
         vlc
       ]
       ++ [
@@ -265,6 +264,13 @@ in
   # You should use the default configuration (which is no configuration), as that works the best out of the box.
   # However, if you need to configure something see https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md for configuration options and https://mynixos.com/nixpkgs/option/services.wivrn.config.json for an example configuration.
   # };
+
+  # services.openssh.settings.PasswordAuthentication = true;
+
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "client";
+  };
 
   programs = {
     steam = {
