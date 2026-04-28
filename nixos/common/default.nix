32266fda536befff2181bb6bfeba804f70f2bd8f
Cole Fuerth <colefuerth@gmail.com>
2026-04-28 13:48:05 -0700

    cuda-maintainers migrated to nixos-cuda in nov 2025

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -51,12 +51,12 @@ in
               max-jobs = "auto";
               substituters = [
                 "https://nix-community.cachix.org"
-                "https://cuda-maintainers.cachix.org"
+                "https://cache.nixos-cuda.org"
                 "https://install.determinate.systems"
               ];
               trusted-public-keys = [
                 "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+                "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
                 "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
               ];
             };
